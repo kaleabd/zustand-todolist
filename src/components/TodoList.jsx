8ac72da34c85a392a00100a75dbbd4eca93fb9f0
kaleabd <kaleabdereje94@gmail.com>
2023-06-28 11:13:14 +0300

    updated the checking checkbox init

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
-import useTodoStore from '../store/TodoStore'
-import deletebtn from '../assets/deletebtn.svg'
+import React from 'react';
+import useTodoStore from '../store/TodoStore';
+import deletebtn from '../assets/deletebtn.svg';
 
 const TodoList = () => {
-    const {todos, removeTodo} = useTodoStore(
-        (state) => ({
-            todos: state.todos,
-            removeTodo: state.removeTodo
-        })
-    )
+  const { todos, removeTodo, toggleIsDone } = useTodoStore((state) => ({
+    todos: state.todos,
+    removeTodo: state.removeTodo,
+    toggleIsDone: state.toggleIsDone
+  }));
+
+  const handleCheckboxChange = (todoId) => {
+    toggleIsDone(todoId);
+  };
+
   return (
     <div className=''>
-        <div>
-            {todos.map((todo, id) => {
-                return (
-                    <div key={id} className="flex justify-between  mb-4 py-2 border-b-2 px-4">
-                        <div>{todo?.title}</div>
-                        
-                            <img 
-                            src={deletebtn}
-                            alt="delete icon"
-                            className='w-7 h-7'
-                            onClick={
-                                () => {
-                                    removeTodo(todo.id)
-                                }
-                            }/>
-                    </div>
-                )
-            })}
-        </div>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id} className="flex justify-between  mb-4 py-2 border-b-2 px-4">
+            <input
+              type="checkbox"
+              checked={todo.isDone}
+              onChange={() => handleCheckboxChange(todo.id)}
+            />
+            <div className={todo.isDone ? 'line-through' : ''}>{todo.title}</div>
+            <img
+              src={deletebtn}
+              alt="delete icon"
+              className='w-7 h-7'
+              onClick={() => {
+                removeTodo(todo.id);
+              }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -2,18 +2,37 @@ import create from "zustand";
 import {devtools, persist} from 'zustand/middleware';
 
 const todoStore = (set) => ({
-    todos: [],
-    addTodo: (todo) => {
-        set(state => ({
-            todos: [todo, ...state.todos]
-        }))
-    },
-    removeTodo: (todoId) => {
-        set((state) => ({
-            todos: state.todos.filter((c) => c.id !== todoId)
-        }))
-    }
-})
+  todos: [],
+  addTodo: (todo) => {
+    set((state) => ({
+      todos: [todo, ...state.todos]
+    }));
+  },
+  removeTodo: (todoId) => {
+    set((state) => ({
+      todos: state.todos.filter((c) => c.id !== todoId)
+    }));
+  },
+/* The `toggleIsDone` function is a method of the `todoStore` object. It is used to toggle the `isDone`
+property of a todo item in the `todos` array. */
+  toggleIsDone: (todoId) => {
+    set((state) => {
+      const updatedTodos = state.todos.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            isDone: !todo.isDone
+          };
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  }
+});
+
 
 const useTodoStore = create(
     devtools(
